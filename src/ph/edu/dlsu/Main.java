d79f9ffcedd7aaa0593a4490917e4e6dc1b22a3d
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-02-16 18:43:22 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,12 @@
+package ph.edu.dlsu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String strValue = null;
+        System.out.println(strValue);
+
+        String[] str = {"Welcome!"};
+        System.out.println(str[1]);
+    }
+}
